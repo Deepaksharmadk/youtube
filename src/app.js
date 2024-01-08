27257a992a91b94827b0b36735481828be2176e6
deepaksharmadk <dks847203@gmail.com>
2024-01-09 00:50:21 +0530

    add user middleware

--- src/app.js
+++ src/app.js
@@ -9,4 +9,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 // Middleware to serve static files from the "public" directory
 app.use(express.static("public"));
+//import user route
+import userRoute from "./routes/user.route.js";
+app.use("/api/v1/users", userRoute);
 export default app;
